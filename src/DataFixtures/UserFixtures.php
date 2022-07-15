c734636962e0967ae9719cd5f8022da59f86a8e9
Hexaku <hex4ku@gmail.com>
2022-07-15 11:29:04 +0200

    create user entity, migration, fixtures and add config in security.yaml

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasherInterface)
+    {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = (new User())
+        ->setEmail('admin@minimal.com')
+        ->setRoles(['ROLE_ADMIN']);
+        $adminPlainPassword = 'admin';
+        $adminHashedPassword = $this->userPasswordHasherInterface->hashPassword($admin, $adminPlainPassword);
+        $admin->setPassword($adminHashedPassword);
+
+        $manager->persist($admin);
+
+        $user = (new User())
+            ->setEmail('user@minimal.com')
+            ->setRoles(['ROLE_USER']);
+        $userPlainPassword = 'user';
+        $userHashedPassword = $this->userPasswordHasherInterface->hashPassword($user, $userPlainPassword);
+        $user->setPassword($userHashedPassword);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
