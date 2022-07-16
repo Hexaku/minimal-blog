55d28a807fd704e1fbae2730aa8b65912f1824b6
Hexaku <hex4ku@gmail.com>
2022-07-16 15:06:03 +0200

    add validators for user entity, display form error messages in template

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,9 +7,11 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -18,6 +20,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $id;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Assert\Email()]
+    #[Assert\NotBlank()]
+    #[Assert\Length(
+        max: 4096,
+        maxMessage: "Your email should not be more than 4096 characters"
+    )]
     private $email;
 
     #[ORM\Column(type: 'json')]
@@ -27,6 +35,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $password;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(
+        min:3,
+        minMessage: "Your username should be at least 3 characters",
+        max: 4096,
+        maxMessage: "Your username should not be more than 4096 characters"
+    )]
     private $username;
 
     public function getId(): ?int
