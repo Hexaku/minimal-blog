a2c39a998a1cb2add2ac78293ebe8f4a69b178df
Hexaku <hex4ku@gmail.com>
2022-07-22 20:06:18 +0200

    add possibility to add a comment on article page

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/posts', name: 'posts_')]
@@ -22,15 +25,29 @@ class PostController extends AbstractController
     }
 
     #[Route('/{slug}', name:'show')]
-    public function show(Post $post, CommentRepository $commentRepository)
+    public function show(Post $post, CommentRepository $commentRepository, Request $request)
     {
         $comments = $commentRepository->findAllLatestCommentsByPost($post->getId());
-        $today = new DateTime();
-        //$today->diff($comments[0]->getCreatedAt()
-        //dd($today->diff($comments[0]->getCreatedAt()));
-        return $this->render('post/show.html.twig', [
+
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+
+        if($commentForm->isSubmitted()){
+            $comment->setAuthor($this->getUser());
+            $comment->setCreatedAt(new DateTime());
+            $comment->setPost($post);
+
+            $commentRepository->add($comment, true);
+
+            return $this->redirectToRoute('posts_show', ['slug' => $post->getSlug()]);
+        }
+
+        return $this->renderForm('post/show.html.twig', [
             'post' => $post,
-            'comments' => $comments
+            'comments' => $comments,
+            'commentForm' => $commentForm,
+            'buttonLabel' => 'Post Comment'
         ]);
     }
 }
