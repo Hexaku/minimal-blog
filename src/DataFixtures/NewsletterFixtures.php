0d6963da933a92ea73b5db6e2bb6309ff1aa1b8a
Hexaku <hex4ku@gmail.com>
2022-07-25 12:04:24 +0200

    add newsletter fixtures, add docker file for mailcatcher and rabbitMQ

---
+++ src/DataFixtures/NewsletterFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Newsletter;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class NewsletterFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for($i=0; $i<10; $i++){
+            $newsletter = (new Newsletter())
+                ->setTitle('Vestibulum cursus rhoncus interdum.')
+                ->setContent('In ornare ligula porta ex vehicula ultrices. Donec fermentum tellus quam, id lobortis dui ullamcorper sed. In tellus ex, condimentum ac mattis ac, fringilla non massa. Integer a sodales eros, in luctus enim. Fusce tortor elit, efficitur ut malesuada ac, euismod ut purus. Donec sed varius elit.');
+            $manager->persist($newsletter);
+        }
+
+        $manager->flush();
+    }
+}
